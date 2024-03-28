8b9ef0ce0c2a6e5d88829cd8d68275453ba994ab
Deepanshuisjod <deepanshudb1@gmail.com>
2024-03-28 17:05:13 +0530

    add/modify server/

---
+++ server/server.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+/*type Handler interface {
+	ServeHTTP(ResponseWriter, *Request)
+}
+type ResponseWriter interface {
+	Header() Header
+	Write([]byte) (int, error)
+	WriteHeader(int)
+}*/
+
+func main() {
+	http.ListenAndServe(":8080", http.FileServer(http.Dir("./as")))
+}
