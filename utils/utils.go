eb1f70f1a1890856eec3947f918c1a64fb893587
Deepanshuisjod <deepanshudb1@gmail.com>
2024-04-08 23:21:07 +0530

    modify utils/utils.go

---
+++ utils/utils.go
@@ -0,0 +1,34 @@
+type utils struct {
+}
+
+func ById() {
+	//
+}
+
+func ByPath() {
+	//
+}
+
+func ByPath() {
+	//
+}
+
+func ByCssSelector() {
+	//
+}
+
+func TagName() {
+	//
+}
+
+func ByLinkText() {
+	//
+}
+
+func ByName() {
+	//
+}
+
+func ByAttr() {
+	//
+}
