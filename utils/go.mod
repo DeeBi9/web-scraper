1acf2a8f7f9a3751854537da0d9f852a7e7ee161
Deepanshuisjod <deepanshudb1@gmail.com>
2024-04-10 19:42:31 +0530

    modify utils.go and add tokernizer package

--- utils/go.mod
+++ utils/go.mod
@@ -1,3 +1,5 @@
 module utils
 
 go 1.21.8
+
+require golang.org/x/net v0.24.0 // indirect
--- utils/utils.go
+++ utils/utils.go
@@ -2,30 +2,90 @@ package utils
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
-
-var url string
-
-func SpecifyUrl(url string) {
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Incorrect url")
-		panic(err)
-	}
-	fmt.Println(resp)
-}
 
 type utils struct {
 	id    string
 	class string
 }
 
-func UseId() {
+var resp *http.Response
+var url string
+
+func parseContent(resp *http.Response) {
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	tokenizer := html.NewTokenizer(strings.NewReader(string(body)))
+	for {
+		tokenType := tokenizer.Next()
+		token := tokenizer.Token()
+		if tokenType == html.ErrorToken {
+			if tokenizer.Err() == io.EOF {
+				return
+			}
+			fmt.Printf("Error: %v", tokenizer.Err())
+			return
+		}
+		fmt.Printf("Token: %v\n", html.UnescapeString(token.String()))
+	}
+}
+
+func noContent(resp *http.Response) {
+	if resp.ContentLength == 0 {
+		fmt.Println("The Response body has no content")
+	}
+}
+
+func clientError() {
 	//
 }
 
-func UsePath() {
+func handleResponse(statusCode string, resp *http.Response) {
+	code_list := strings.Split(statusCode, " ")
+	code := code_list[0]
+
+	switch {
+	case code == "200": // The request is successful
+		parseContent(resp)
+	case code == "204": // Server successfully processed the request but has no content
+		noContent(resp)
+	case code == "301":
+		//
+	case code == "302":
+		//
+	case code == "304":
+		//
+	case code == "400": // Cannot process due to client error
+		clientError()
+	case code == "401":
+		// Unauthorized
+	case code == "403":
+		// Forbidden
+	case code == "404":
+		// Not Found
+	}
+
+}
+
+func Url(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Incorrect url")
+		panic(err)
+	}
+
+}
+
+func UseId() {
 	//
 }
 
